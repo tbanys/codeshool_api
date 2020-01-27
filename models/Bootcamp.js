b738f352f0702303fc69b52fa0816cb021d996f9
Tautvydas <banys.tautvydas@gmail.com>
2020-01-27 21:48:22 +0200

    CRUD Updated

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -97,11 +97,6 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now
     },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
-    }
   },
   {
     toJSON: { virtuals: true },
--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
