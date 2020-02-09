cf2196900cca4a2deb1ba251213dfb329df93390
Tautvydas <banys.tautvydas@gmail.com>
2020-02-09 12:04:41 +0200

    logout with cookies

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,9 +8,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
   } 
   // else if (req.cookies.token) {
+  //   // Set token from cookie
   //   token = req.cookies.token;
   // }
 
